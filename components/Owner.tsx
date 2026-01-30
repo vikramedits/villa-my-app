ca2965f1aafb7868e8d993cb75697869cc47a6c0
Vikram Singh <vikram.deora@woodenstreet.com>
2026-01-30 17:47:53 +0530

    images added to home

--- components/Owner.tsx
+++ components/Owner.tsx
@@ -45,6 +45,7 @@ export default function OwnersNote() {
   return (
     <section ref={sectionRef} className=" py-8 md:py-12">
       <div className="container-fluid">
+        
         <div className="pb-4 md:pb-8">
           <p className="text-lg md:text-2xl font-bold md:font-medium tracking-wide text-gray-950 ">
             Note By Host & Co-Host
@@ -55,19 +56,18 @@ export default function OwnersNote() {
           </p>
         </div>
 
-        <div className="max-w-6xl mx-auto flex flex-col md:flex-row gap-6 md:gap-10 justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-10 justify-center">
           {OWNERS.map((owner, idx) => (
             <div
               key={idx}
               className={`
               bg-white border rounded-3xl p-6 md:p-10 shadow-lg
-              md:flex md:items-center gap-6 transition-all duration-700 ease-out
+             transition-all duration-700 ease-out
               ${visible ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}
             `}
-              style={{ transitionDelay: `${idx * 150}ms` }}
-            >
-              {/* Image */}
-              <div className="shrink-0 mx-auto md:mx-0 flex gap-10 items-center">
+              style={{ transitionDelay: `${idx * 150}ms` }}>
+
+              <div className="shrink-0 mx-auto md:mx-0 flex gap-10 md:gap-20 items-center">
                 <div className="w-24 h-24 md:w-32 md:h-32 rounded-full overflow-hidden border border-primaryBlue">
                   <Image
                     src={owner.image}
@@ -77,8 +77,8 @@ export default function OwnersNote() {
                     className="object-cover"
                   />
                 </div>
+
                 <div>
-                  {/* Name & Role */}
                   <p className="mt-4 text-primaryBlue font-bold text-base md:text-lg uppercase">
                     {owner.name}
                   </p>
@@ -88,16 +88,13 @@ export default function OwnersNote() {
                 </div>
               </div>
 
-              {/* Text Content */}
-              <div className="mt-4 md:mt-0 text-center md:text-left flex-1 relative">
-                {/* Speech-bubble tail */}
+              <div className="mt-4 md:mt-8 text-center md:text-left flex-1 relative">
                 <div className="absolute -top-3 left-1/2 -translate-x-1/2 w-5 h-5 bg-white rotate-45 md:hidden"></div>
-
-                {/* Message */}
                 <p className="text-gray-700 text-sm md:text-base leading-relaxed">
                   {owner.message}
                 </p>
               </div>
+
             </div>
           ))}
         </div>
