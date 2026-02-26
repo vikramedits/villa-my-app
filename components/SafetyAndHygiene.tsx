d69662b39cb68485de62ab26502d3304c3375fb3
Vikram Singh <vikramedits2k26@gmail.com>
2026-02-26 11:21:21 +0530

    safety & hygine

--- components/SafetyAndHygiene.tsx
+++ components/SafetyAndHygiene.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { ArrowLeftToLine, ArrowUpToLine } from "lucide-react";
 import Image from "next/image";
 import { useState, useEffect } from "react";
+import { itemAxisPredicate } from "recharts/types/state/selectors/axisSelectors";
 
 const SAFETY_DATA = [
   {
@@ -51,7 +53,12 @@ const SAFETY_DATA = [
 export default function SafetyAndHygiene() {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
   const [radius, setRadius] = useState(260);
+  const [showTap, setShowTap] = useState(false);
 
+  const handleCardClick = (index: number) => {
+    setOpenIndex(index);
+    setShowTap(true); // Pehli card select ke baad Tap dikhana
+  };
   // Responsive radius handler
   useEffect(() => {
     const updateRadius = () => {
@@ -72,26 +79,45 @@ export default function SafetyAndHygiene() {
       <div className="container-fluid mx-auto px-4">
         {/* ============= Heading ============== */}
         <div className="mb-16 text-start">
-          <p className="text-2xl md:text-3xl font-bold border-l-4 border-black inline-block pl-3">
+          <p className="text-lg md:text-2xl font-bold md:font-medium tracking-wide text-gray-950 border-l-4 border-black pl-2">
             Safety & Hygiene
           </p>
-          <p className="text-gray-600 mt-3">
+          <p className="text-xs md:text-base mt-2 text-gray-600 tracking-wide">
             Your comfort, safety and cleanliness are our top priority
           </p>
         </div>
         {/* ============= Circular Layout ==================== */}
         <div className="flex flex-col md:flex-row items-start md:items-center gap-8 md:gap-16">
           {/* Left: Circular Layout */}
-          <div className="relative w-full md:flex-1 flex items-center justify-center h-96 md:h-[750px]">
+          <div className="relative w-full md:flex-1 flex items-center justify-center h-96 md:h-187.5">
             {/* Center Image */}
-            <div className="absolute w-52 h-52 md:w-80 md:h-80 rounded-2xl overflow-hidden shadow-2xl z-0">
+            <div
+              className="absolute w-52 h-52 md:w-80 md:h-80 rounded-2xl overflow-hidden shadow-2xl z-0 cursor-pointer animate-float"
+              onClick={() => {
+                setOpenIndex(null);
+                setShowTap(true);
+                setShowTap((prev) => !prev); // first click pe Tap overlay dikhana
+              }}
+            >
               <Image
                 src="/homenew/standard/Room-home-image.jpeg"
                 alt="Villa"
                 fill
-                className="object-cover"
+                className="object-cover transition-transform duration-300 ease-in-out  hover:scale-110"
                 priority
               />
+
+              {/* Tap overlay */}
+              {showTap && (
+                <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+                  <p
+                    className="text-white font-medium text-center text-sm md:text-lg bg-black bg-opacity-30 px-3 border border-green-500 py-1 rounded-full
+                  "
+                  >
+                    Tap
+                  </p>
+                </div>
+              )}
             </div>
 
             {/* Cards */}
@@ -109,8 +135,9 @@ export default function SafetyAndHygiene() {
                 >
                   <div className="flex flex-col items-center">
                     <div
-                      className="w-28 h-28 md:w-44 md:h-44 bg-white shadow-lg rounded-xl flex flex-col items-center justify-center p-3 md:p-4 cursor-pointer hover:scale-105 transition"
-                      onClick={() => setOpenIndex(index)}
+                      className="w-28 h-28 md:w-44 md:h-44 bg-white shadow-lg rounded-xl flex flex-col items-center justify-center p-3 md:p-4 cursor-pointer
+                    border  hover:border-green-700 hover:scale-110 duration-200 transition"
+                      onClick={() => handleCardClick(index)}
                     >
                       <div className="relative w-8 h-8 md:w-12 md:h-12 mb-2 md:mb-3">
                         <Image
@@ -132,16 +159,40 @@ export default function SafetyAndHygiene() {
 
           {/* Right: Info Box */}
           <div className="w-full md:flex-1">
-            {openIndex !== null && (
-              <div className="w-full bg-black text-white p-6 rounded-xl shadow-2xl animate-fadeIn h-40 md:h-auto overflow-y-auto">
-                <p className="font-bold text-lg md:text-xl mb-2">
-                  {SAFETY_DATA[openIndex].title}
-                </p>
-                <p className="text-sm md:text-base">
-                  {SAFETY_DATA[openIndex].description}
-                </p>
-              </div>
-            )}
+            <div className="w-full bg-black text-white p-6 rounded-xl shadow-2xl animate-fadeIn h-40 md:h-auto overflow-y-auto">
+              {openIndex === null ? (
+                <>
+                  <ArrowUpToLine className="block lg:hidden mx-auto mb-4 w-8 h-8" />
+                  <ArrowLeftToLine className="hidden lg:block mx-auto mb-4 w-10 h-10" />
+                  <p className="text-sm md:text-base text-center uppercase">
+                    Please click a <span className="text-green-500">card</span>{" "}
+                    to see the{" "}
+                    <span className="text-green-500">information</span> !
+                  </p>
+                </>
+              ) : (
+                <div className="flex items-center gap-8 lg:gap-8">
+                  <div className="w-20 h-8 md:w-12 md:h-12 ">
+                    <Image
+                      width={64}
+                      height={64}
+                      src={SAFETY_DATA[openIndex].icon}
+                      alt="icon"
+                      className=" object-cover aspect-square rounded-sm"
+                    />
+                  </div>
+
+                  <div>
+                    <p className="font-bold text-lg md:text-xl mb-2">
+                      {SAFETY_DATA[openIndex].title}
+                    </p>
+                    <p className="text-sm md:text-base">
+                      {SAFETY_DATA[openIndex].description}
+                    </p>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
